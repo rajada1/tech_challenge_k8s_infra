e8cb5a848aafe22afc9e86ce838ee1f135fe5733
rajada1 <matheusiasd@hotmail.com>
2025-11-27 06:31:20 -0300

    fix: update S3 backend key and bucket tag for consistency

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tfstate-backend-fiap-1213"
-    key    = "terraform.tfstate"
+    key    = "tech_challenge_k8s_infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "bucket-backend" {
   bucket = "tfstate-backend-fiap-1213"
   tags = {
-    Name        = "tfstate backend k8s Challenge"
+    Name        = "tfstates_tech_challenge"
     Environment = "Production"
   }
   
