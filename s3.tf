acbad5a4ec39fa466cfa12ffbed34f851e6e8ba7
rajada1 <matheusiasd@hotmail.com>
2025-11-28 00:01:05 -0300

    fix: ensure force_destroy is enabled for S3 backend bucket

--- s3.tf
+++ s3.tf
@@ -1,9 +1,10 @@
 resource "aws_s3_bucket" "bucket-backend" {
   bucket = "tfstate-backend-fiap-1213"
+  force_destroy = true
+
   tags = {
     Name        = "tfstates_tech_challenge"
     Environment = "Production"
   }
-  
 }
 
