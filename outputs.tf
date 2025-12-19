f65b20833eb6691b1f16f6bc87f9ca97d349be69
rajada1 <matheusiasd@hotmail.com>
2025-12-19 11:28:33 -0300

    feat: Add manual deployment and destruction workflows with environment selection, and store additional EKS/VPC outputs in SSM.

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,18 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = join(",", module.vpc.private_subnets)
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
